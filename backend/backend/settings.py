57576ee5ed7dc7710d3013ee526f83831771d857
eobgolts <eobgolts@gmail.com>
2023-11-23 16:16:54 +0300

    Add actions

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -1,5 +1,4 @@
 import os
-
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -7,7 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^'
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.143.124', 'taski.obgolts.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '84.201.143.124', 'eobgolts-taski.ddns.net']
 
 
 # Application definition
